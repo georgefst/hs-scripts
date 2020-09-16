8e99f6109f368f9930114e81e6c72b362e4fa89a
George Thomas <georgefsthomas@gmail.com>
2020-09-16 02:20:36 +0100

    Use cabal-env
    'install --lib' is just utterly broken: https://github.com/haskell/cabal/issues/5559

--- cab-install-lib.sh
+++ cab-install-lib.sh
@@ -1,17 +1,46 @@
 #!/bin/bash
 
-rm .ghc.env*
+#TODO move this stuff in to Build.hs
 
-cabal install --package-env . --lib \
-    ansi-terminal-0.11 \
-    directory-1.3.6.1 \
-    extra-1.7.7 \
-    lucid-2.9.12 \
-    optparse-generic-1.4.3 \
-    pretty-simple-4.1.0.0 \
-    prettyprinter-1.7.0 \
-    prettyprinter-lucid-0.1.0.1 \
-    safe-0.3.19 \
-    shake-0.19.1 \
-    time-1.9.3 \
-    turtle-1.5.21 \
+SCRIPTS_DIR=$(pwd)
+ARCH_VER=x86_64-linux-8.10.2
+ENV_DIR=/home/gthomas/.ghc/$ARCH_VER/environments/
+
+rm $ENV_DIR/scripts
+
+rm -rf git-deps
+mkdir git-deps
+
+function cabal-env-git {
+    cd git-deps
+    git clone $1
+    cd $2
+    git checkout $4
+    cabal-env --local $3 -n scripts
+    cd $SCRIPTS_DIR
+}
+
+cabal-env-git https://github.com/cdepillabout/pretty-simple pretty-simple pretty-simple ae8d2f17ba78edba02b83aed9296a461ebec5f79
+cabal-env-git https://github.com/georgefst/evdev evdev/evdev evdev a3959e51d3e4274ca96c945e81ef7cbb0453511e
+
+#TODO versions - not sure currently possible with cabal-env
+    # (but in that case what does '--any' mean?)
+cabal-env -n scripts \
+    ansi-terminal \
+    bytestring \
+    directory \
+    extra \
+    filepath \
+    ghc \
+    lucid \
+    optparse-generic \
+    prettyprinter \
+    prettyprinter-lucid \
+    process \
+    safe \
+    shake \
+    text \
+    time \
+    unix \
+
+cp $ENV_DIR/scripts $SCRIPTS_DIR/.ghc.environment.$ARCH_VER
