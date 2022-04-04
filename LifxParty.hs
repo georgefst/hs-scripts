6210e727d83955f6009e7023ddbb57ac34e31c4e
George Thomas <georgefsthomas@gmail.com>
2022-04-04 18:18:47 +0100

    Add LIFX party mode script
    This is a squash merge of the lifx-party branch. This is a good time to merge since I've just released lifx-lan-0.7, so we no longer require a patched version.
    
    Merged commits are:
    
    commit 02339644c6d382bc6c5acff1bb62ecc6a28a8442
    Author: George Thomas <georgefsthomas@gmail.com>
    Date:   Sun Apr 3 21:03:40 2022 +0100
    
        Remove git overrides
    
    commit 5c8d4396c95f1bc49c4ed6fbba8f5ddd68ce7ac9
    Author: George Thomas <georgefsthomas@gmail.com>
    Date:   Sun Apr 3 20:57:40 2022 +0100
    
        Add lib
    
    commit 198db8a6539ef8c7d77c89d6c2be06a601a64d55
    Author: George Thomas <georgefsthomas@gmail.com>
    Date:   Mon Mar 14 10:42:02 2022 +0000
    
        Simplify by using `sendMessageAndWait`
    
    commit 15aaa866dacbcaf44ae1dd6732033a0149f9035f
    Author: George Thomas <georgefsthomas@gmail.com>
    Date:   Mon Mar 14 10:29:45 2022 +0000
    
        Formatting
    
    commit bfd6c9de63a8d4182eb8ad0856ffc93e356041fe
    Merge: 307d172 a8a7186
    Author: George Thomas <georgefsthomas@gmail.com>
    Date:   Mon Mar 14 09:09:50 2022 +0000
    
        Merge branch 'master' into lifx-party
    
    commit 307d172bf6d0fe7441a99adf89c959b3fefa9ccc
    Author: George Thomas <georgefsthomas@gmail.com>
    Date:   Mon Mar 14 00:12:22 2022 +0000
    
        Update commit hash for `lifx-lan`
    
    commit 3d83d275f2877cc9e2a63f9cfe5d72e4c3bfabdb
    Author: George Thomas <georgefsthomas@gmail.com>
    Date:   Sun Mar 13 17:21:59 2022 +0000
    
        Add comment on why we can't use Hackage `lifx-lan`
    
    commit 082d56010bf6d113b20f534c3da7f4c2a5419002
    Merge: 854263c 4e6622a
    Author: George Thomas <georgefsthomas@gmail.com>
    Date:   Sat Feb 26 10:54:08 2022 +0000
    
        Merge branch 'master' into lifx-party
    
    commit 854263c7870823dbdb2ac02f0113261bb266474a
    Author: George Thomas <georgefsthomas@gmail.com>
    Date:   Sat Feb 26 10:46:54 2022 +0000
    
        Simplify candle mode
    
    commit 07a6507102dff36401e3b4eb92f052fd74dc0dd7
    Author: George Thomas <georgefsthomas@gmail.com>
    Date:   Sat Feb 26 10:45:06 2022 +0000
    
        Add candle mode
    
        As run for an hour or so last night, at Ethan's request.
    
    commit b054fa006f247a4dcdcf15d90202a8c15fe61dee
    Author: George Thomas <georgefsthomas@gmail.com>
    Date:   Sun Feb 20 14:34:59 2022 +0000
    
        Use latest lifx-lan, with mocking support
    
    commit 15fbaf04fc7df3e19b610669e5b496a1d8cb66c5
    Author: George Thomas <georgefsthomas@gmail.com>
    Date:   Sun Feb 20 14:22:28 2022 +0000
    
        Formatting
    
    commit 0b89374d7ae1f920df937bfae485bd9634d093ce
    Author: George Thomas <georgefsthomas@gmail.com>
    Date:   Fri Feb 18 10:54:48 2022 +0000
    
        Add `colour` lib
    
    commit 2d1d456a3cb803a2d460f3bca408194744d91c8b
    Author: George Thomas <georgefsthomas@gmail.com>
    Date:   Thu Feb 17 12:50:08 2022 +0000
    
        Simplify language pragmas
    
    commit 2235c0c92a57f22b00be56c32e4540f6197fb05a
    Author: George Thomas <georgefsthomas@gmail.com>
    Date:   Thu Feb 17 12:49:55 2022 +0000
    
        Correct module name
    
    commit 7fbaff491c874fcf7621e95eee84cfb2c485f0eb
    Author: George Thomas <georgefsthomas@gmail.com>
    Date:   Sun Feb 13 21:56:28 2022 +0000
    
        More principled numeric conversions
    
    commit 0860347b8d5e377ce1371bcb1e823602a5b48672
    Author: George Thomas <georgefsthomas@gmail.com>
    Date:   Sun Feb 13 00:57:47 2022 +0000
    
        Just use old-style global generator
    
    commit ae33b922012f2daaaa11982e98976e27630b1312
    Author: George Thomas <georgefsthomas@gmail.com>
    Date:   Sun Feb 13 00:56:56 2022 +0000
    
        Use state monad
    
    commit 1c3aca564da8a796ec6d568e57035f3f357f0d49
    Author: George Thomas <georgefsthomas@gmail.com>
    Date:   Sun Feb 13 00:56:13 2022 +0000
    
        lifx-party

---
+++ LifxParty.hs
@@ -0,0 +1,47 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE GHC2021 #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
+{-# OPTIONS_GHC -Wall #-}
+{-# OPTIONS_GHC -Wno-missing-signatures #-}
+{-# OPTIONS_GHC -fdefer-type-errors #-}
+
+module LifxParty where
+
+import Control.Concurrent
+import Control.Monad
+import Control.Monad.IO.Class
+import Data.Time
+import Lifx.Lan
+import Lifx.Lan.Mock.Terminal
+import System.Random.Stateful
+
+dev = deviceFromAddress (192, 168, 1, 71)
+
+main = runLifx party
+
+mock = runMock [(dev, "Lamp")] party
+party = forever do
+    hue <- uniformM globalStdGen
+    let color =
+            HSBK
+                { hue
+                , saturation = maxBound
+                , brightness = maxBound
+                , kelvin = maxBound
+                }
+    sendMessageAndWait dev $ SetColor color $ secondsToNominalDiffTime 0.5
+
+candle = do
+    let color =
+            HSBK
+                { hue = 0
+                , saturation = 0
+                , brightness = 55978
+                , kelvin = 2328
+                }
+    sendMessage dev $ SetColor color 0
+    forever do
+        sendMessage dev . SetPower =<< randomIO
+        liftIO $ threadDelay 10_000
