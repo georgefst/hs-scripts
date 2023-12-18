a27a3d1eec1a533e274cb0fa3e857d8494ed4840
George Thomas <georgefsthomas@gmail.com>
2023-12-18 21:42:18 +0000

    aoc 5 part 1 solved

--- AoC5.hs
+++ AoC5.hs
@@ -1,7 +1,9 @@
 {- HLINT ignore "Unused LANGUAGE pragma" -}
 {-# LANGUAGE GHC2021 #-}
 {-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedRecordDot #-}
@@ -22,11 +24,13 @@ import Control.Applicative hiding (many, some) -- TODO bit weird - why hide?
 import Control.Monad
 import Control.Monad.IO.Class
 import Data.Foldable qualified as F
+import Data.List
 import Data.List.NonEmpty (NonEmpty ((:|)))
 import Data.Text (Text)
 import Data.Text.IO qualified as T
 import Data.Text.Lazy qualified as TL
 import Data.Void
+import GHC.TypeLits (Symbol)
 import Text.Megaparsec hiding (sepBy1)
 import Text.Megaparsec.Char
 import Text.Pretty.Simple
@@ -36,33 +40,48 @@ main = do
     _example <- either (error . TL.unpack . pShow) id . runParser parser "" <$> T.readFile "aoc5-example"
     input <- either (error . TL.unpack . pShow) id . runParser parser "" <$> T.readFile "aoc5-input"
     putStrLn ""
-    pp _example
-    pp input
+    pp $ minimum $ map (lookupTrans input) input.seeds
 
+lookupTrans :: Input -> ID "seed" -> ID "location"
+lookupTrans Input{..} =
+    lookupID humidityToLocation
+        . lookupID temperatureToHumidity
+        . lookupID lightToTemperature
+        . lookupID waterToLight
+        . lookupID fertilizerToWater
+        . lookupID soilToFertilizer
+        . lookupID seedToSoil
+
+lookupID :: [Maplet a b] -> ID a -> ID b
+lookupID = flip \(ID x) ->
+    maybe (ID x) (\Maplet{..} -> ID $ destinationRangeStart.unwrap + x - sourceRangeStart.unwrap)
+        . find \Maplet{..} -> sourceRangeStart.unwrap <= x && sourceRangeStart.unwrap + rangeLength > x
+
+-- TODO remove the repetitiveness of all the separate maps somehow?
 data Input = Input
-    { seeds :: [Int]
-    , seedToSoil :: [Maplet]
-    , soilToFertilizer :: [Maplet]
-    , fertilizerToWater :: [Maplet]
-    , waterToLight :: [Maplet]
-    , lightToTemperature :: [Maplet]
-    , temperatureToHumidity :: [Maplet]
-    , humidityToLocation :: [Maplet]
+    { seeds :: [ID "seed"]
+    , seedToSoil :: [Maplet "seed" "soil"]
+    , soilToFertilizer :: [Maplet "soil" "fertilizer"]
+    , fertilizerToWater :: [Maplet "fertilizer" "water"]
+    , waterToLight :: [Maplet "water" "light"]
+    , lightToTemperature :: [Maplet "light" "temperature"]
+    , temperatureToHumidity :: [Maplet "temperature" "humidity"]
+    , humidityToLocation :: [Maplet "humidity" "location"]
     }
     deriving (Show)
-
-data Maplet = Maplet
-    { destinationRangeStart :: Int
-    , sourceRangeStart :: Int
+data Maplet a b = Maplet
+    { sourceRangeStart :: ID a
+    , destinationRangeStart :: ID b
     , rangeLength :: Int
     }
     deriving (Show)
+newtype ID (s :: Symbol) = ID {unwrap :: Int} deriving newtype (Eq, Ord, Show)
 
 type Parser = Parsec Void Text
 
 parser :: Parser Input
 parser = do
-    seeds <- "seeds: " *> sepBy1 int " "
+    seeds <- "seeds: " *> sepBy1 (ID <$> int) " "
     line
     seedToSoil <- f "seed-to-soil"
     soilToFertilizer <- f "soil-to-fertilizer"
@@ -75,15 +94,16 @@ parser = do
     eof
     pure Input{..}
   where
+    f :: Text -> Parser [Maplet a b]
     f s = do
         line
         space
         void $ string s <> " map:"
         line
         flip sepBy1 newline do
-            destinationRangeStart <- int
+            destinationRangeStart <- ID <$> int
             space
-            sourceRangeStart <- int
+            sourceRangeStart <- ID <$> int
             space
             rangeLength <- int
             pure $ Maplet{..}
@@ -91,7 +111,7 @@ parser = do
     line = void newline
 
 pp :: (MonadIO m, Show a) => a -> m ()
-pp = pPrintForceColor
+pp = pPrintOpt NoCheckColorTty defaultOutputOptionsDarkBg{outputOptionsCompact = True}
 
 -- TODO this is a real pain-point - https://stackoverflow.com/questions/60222934/avoid-parsing-last-separator-with-sepby
 sepBy1 :: (Alternative m, MonadPlus m, MonadParsec e0 s0 m) => m a -> m sep -> m [a]
