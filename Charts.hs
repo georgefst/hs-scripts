e45916338610f6e9f7f928ed9ff3f0ef0f98295c
George Thomas <georgefsthomas@gmail.com>
2025-03-15 20:54:41 +0000

    Add Chart example
    There's a lot here that I otherwise forget every time I reach for the `Chart` library, including to some extent the basics of setting up axes etc. This is particularly frustrating as it's the kind of library one often wants to just quickly reach for in order to try something.
    
    The `diagrams` backend is too slow for live reloading. See https://github.com/timbod7/haskell-chart/issues/105.
    
    Unicode support is poor (in both backends), hence the use of lambda syntax rather than `↦`.
    
    We use `+ 0.001` as a hack to prevent discontinuities for some common functions.

---
+++ Charts.hs
@@ -0,0 +1,38 @@
+{-# LANGUAGE GHC2024 #-}
+{-# LANGUAGE BlockArguments #-}
+{-# OPTIONS_GHC -Wall #-}
+{-# OPTIONS_GHC -Wno-missing-signatures #-}
+{-# OPTIONS_GHC -Wno-type-defaults #-}
+
+module Charts (main) where
+
+import Control.Monad
+import Data.Foldable
+import Graphics.Rendering.Chart
+import Graphics.Rendering.Chart.Backend.Cairo
+import Graphics.Rendering.Chart.Easy hiding (tan)
+
+main :: IO ()
+main = do
+    void
+        . renderableToFile
+            (def & fo_format .~ SVG)
+            "/tmp/chart.svg"
+        . fillBackground def
+        $ toRenderable chart
+
+chart :: Layout Double Double
+chart = execEC $ do
+    layout_x_axis . laxis_generate .= scaledAxis def (lower, upper)
+    for_ functions \(l, f) ->
+        plot $ line l [[(x, f x) | x <- [lower + 0.001, lower + ((upper - lower) / increments) .. upper]]]
+
+increments = 500
+(lower, upper) = (-10, 10)
+
+functions =
+    [ ("sin", \x -> sin x)
+    , ("cos", \x -> cos x)
+    , ("λx. x³/1000", \x -> x ** 3 / 1000)
+    , ("λx. (x ** 2)/200 - x/20 - 1/2", \x -> (x ** 2) / 200 - x / 20 - 1 / 2)
+    ]
