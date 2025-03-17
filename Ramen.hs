2cbced1212a0da5d155c2b67535527b3be097459
George Thomas <georgefsthomas@gmail.com>
2025-03-17 16:39:39 +0000

    better API - still not working

--- Ramen.hs
+++ Ramen.hs
@@ -2,9 +2,13 @@
 {-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE BlockArguments #-}
 {-# LANGUAGE CPP #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE OverloadedRecordDot #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PolyKinds #-}
 {-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE NoFieldSelectors #-}
 {-# LANGUAGE NoMonomorphismRestriction #-}
 {-# OPTIONS_GHC -Wall #-}
 {-# OPTIONS_GHC -Wno-missing-signatures #-}
@@ -23,10 +27,11 @@ import GHC.List (List)
 import GHC.TypeLits (KnownSymbol, Symbol, symbolVal)
 import Language.Javascript.JSaddle qualified as JSaddle
 import Miso qualified
-import Miso.String
+import Miso.String hiding (map)
 
--- TODO some constructors shouldn't be exported, and all synonyms replaced by opaque newtypes
--- (with derived instances, e.g. `MonadState` for `Transition`)
+-- TODO boundaries may not quite be right
+-- e.g. some of this may be useful for interfacing with existing Miso code
+-- also, some constructors shouldn't be exported
 {- API -}
 
 -- TODO rename `env` to `parentStates`? incl. elsewhere
@@ -39,48 +44,34 @@ data Component (name :: Symbol) model action env = Component
 
 -- TODO what about `scheduleIO_`?
 scheduleIO :: JSM name env action -> Transition name model action env Empty
-scheduleIO x = do
-    p <- ask
-    lift $ fmap fromUnit $ Miso.scheduleIO $ flip runReaderT p x
+scheduleIO = fmap fromUnit . Transition . Miso.scheduleIO . (.unwrap)
 
--- TODO I feel like we should be able to avoid manually passing around the env...
--- or maybe we can encapsulate it somehow?
 embed ::
     forall env n m a name model action.
     (Eq m, KnownSymbol n, KnownSymbol name) =>
-    Env env -> Component n m a ('(name, model) ': env) -> View name model action env
-embed e c =
-    Miso.embed $
-        toComponent
-            ( EnvCons
-                ( fmap (error "readEnv: parent not found") -- this can't happen since if a component is mounted so is its parent
-                -- TODO what's the concurrency story here
-                    . Miso.sample
-                    $ Miso.Component
-                        (ms (symbolVal (Proxy @name)))
-                        -- TODO I'm not as certain about this being unused as for `mail`/`notify` - will find out when we test
-                        (error "readEnv: no parent app")
-                )
-                e
-            )
-            c
+    Component n m a ('(name, model) ': env) -> View name model action env
+embed = View . Miso.embed . toComponent
 
-type Transition name model action env = ReaderT (Env env) (Miso.Transition action model)
+newtype Transition name model action env a
+    = Transition {unwrap :: Miso.Transition action model a}
+    deriving newtype (Functor, Applicative, Monad, MonadState model)
 
 -- type JSM (name :: Symbol) env a = (Env env) -> JSaddle.JSM a
-type JSM (name :: Symbol) env a = ReaderT (Env env) JSaddle.JSM a
+newtype JSM (name :: Symbol) env a
+    = JSM {unwrap :: JSaddle.JSM a}
+    deriving newtype (Functor, Applicative, Monad)
 
-type View name model action env = Miso.View action
+newtype View name model action env
+    = View {unwrap :: Miso.View action}
 
 run :: (Eq model) => Component name model action '[] -> IO ()
-run = Miso.run . Miso.startApp . toApp EnvNil
+run = Miso.run . Miso.startApp . toApp
 
--- TODO boundaries may not quite be right - e.g. some of this may be useful for interfacing with existing Miso code
 {- Internal -}
 
 data Env (env :: List (Symbol, Type)) where
     EnvNil :: Env '[]
-    EnvCons :: JSaddle.JSM a -> Env env -> Env ('(s, a) ': env)
+    EnvCons :: Env env -> Env ('(s, a) ': env)
 
 class EnvHas (name :: Symbol) a (env :: List (Symbol, Type)) where
     getFromEnv :: (KnownSymbol name) => JSM name' env a
@@ -89,19 +80,35 @@ class EnvHas (name :: Symbol) a (env :: List (Symbol, Type)) where
 -- use efficient heterogenous map?
 -- can that also be used to rule out duplicate component names?
 instance EnvHas name a ('(name, a) : env) where
-    getFromEnv = ReaderT \case
-        EnvCons a _ -> a
+    getFromEnv =
+        fmap (error "readEnv: parent not found") -- this can't happen since if a component is mounted so is its parent
+            . JSM
+            . Miso.sample
+            $ Miso.Component
+                (ms (symbolVal (Proxy @name)))
+                -- TODO I'm not as certain about this being unused as for `mail`/`notify` - will find out when we test
+                (error "readEnv: no parent app")
 
-toApp :: forall name model action env. Env env -> Component name model action env -> Miso.App model action
-toApp e c =
+toApp :: forall name model action env. Component name model action env -> Miso.App model action
+toApp c =
     Miso.defaultApp
         c.initialState
-        (\a m -> Miso.fromTransition (flip runReaderT e $ fmap toUnit $ c.update a) m)
-        (\m -> c.view m)
+        (\a m -> Miso.fromTransition (fmap toUnit $ (.unwrap) $ c.update a) m)
+        (\m -> (.unwrap) $ c.view m)
         c.initialAction
 
-toComponent :: (KnownSymbol name) => Env env -> Component name model app env -> Miso.Component name model app
-toComponent e = Miso.component . toApp e
+toComponent :: (KnownSymbol name) => Component name model app env -> Miso.Component name model app
+toComponent = Miso.component . toApp
+
+-- TODO wrap `Miso.Attribute`
+div_ :: [Miso.Attribute action] -> [View name model action env] -> View name model action env
+div_ as vs = View $ Miso.div_ as $ map (.unwrap) vs
+button_ :: [Miso.Attribute action] -> [View name model action env] -> View name model action env
+button_ as vs = View $ Miso.button_ as $ map (.unwrap) vs
+text :: MisoString -> View name model action env
+text t = View $ Miso.text t
+onClick :: action -> Miso.Attribute action
+onClick = Miso.onClick
 
 -- TODO this is just because of `NoListTuplePuns` - isolate that I guess, or use new non-un variants
 -- requires `ghc-experimental`: https://ghc-proposals.readthedocs.io/en/latest/proposals/0475-tuple-syntax.html
@@ -109,36 +116,35 @@ data Empty = Empty
 toUnit Empty = ()
 fromUnit () = Empty
 
--- TODO still uses some Miso stuff - we should wrap `div_` etc.
 {- Example -}
 
 main :: IO ()
-main = run topComponent
+main = run top
 
-topComponent :: forall env. Component "top" Word () env
-topComponent =
+-- top :: forall env.  Component "top" Word () env
+top :: forall (env :: List (Symbol, Type)). Component "top" Word () env
+top =
     Component
         { initialState = 0
         , view = \m ->
-            Miso.div_
+            div_
                 []
-                [ Miso.button_ [Miso.onClick ()] [Miso.text $ ms m]
-                , embed EnvNil $ sub @'[ '("top", Word)]
-                -- , embed _ $ sub @( '("top", Word) : env)
+                [ button_ [onClick ()] [text $ ms m]
+                , embed sub
                 ]
         , update = \() -> fromUnit <$> modify (+ 1)
         , initialAction = ()
         }
 
-sub :: (EnvHas "top" Word env) => Component "sub" Word (Maybe Word) env
+sub :: forall env. (EnvHas "top" Word env) => Component "sub" Word (Maybe Word) env
 sub =
     Component
         { initialState = 0
-        , view = \parentState -> Miso.text $ ms parentState
+        , view = \parentState -> text $ ms parentState
         , update = \case
             Nothing -> do
                 (toUnit -> ()) <- scheduleIO do
-                    n <- getFromEnv @"top" @Word
+                    -- n <- getFromEnv @"top" @Word
                     -- pure $ Just n
                     pure Nothing
                 pure $ fromUnit ()
