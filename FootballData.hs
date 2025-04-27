7cd6eac21287cc010e26430dcb8ea61037abde10
George Thomas <georgefsthomas@gmail.com>
2025-04-27 20:19:30 +0100

    Generate league table from data
    There's some serious repetition in `processMatches`, but it's not trivial to get rid of.
    
    The `:: TableRowRaw` annotations in updates are due to a ridiculous state of affairs that stems from delays in the full release of `-XOverloadedRecordUpdate`.

--- FootballData.hs
+++ FootballData.hs
@@ -1,5 +1,8 @@
 {-# LANGUAGE GHC2024 #-}
 {-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DisambiguateRecordFields #-}
+{-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE OverloadedRecordDot #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE PartialTypeSignatures #-}
@@ -8,44 +11,111 @@
 {-# LANGUAGE NoFieldSelectors #-}
 {-# LANGUAGE NoMonomorphismRestriction #-}
 {-# OPTIONS_GHC -Wall #-}
+{-# OPTIONS_GHC -Wno-ambiguous-fields #-}
 
--- | For processing data from https://www.football-data.co.uk.
+{- | For processing data from https://www.football-data.co.uk.
+
+Limitations:
+- Points deductions not accounted for.
+- Tie breakers after goal difference not applied.
+-}
 module FootballData (main) where
 
 import Data.Bifunctor
 import Data.ByteString.Lazy qualified as BL
 import Data.Csv
 import Data.Foldable
+import Data.List (sortOn)
+import Data.Map qualified as Map
 import Data.Maybe
+import Data.Ord (Down (Down))
 import Data.Text (Text)
-import Data.Text qualified as T
 import Data.Text.IO qualified as T
 import Data.Time
+import Data.Tuple.Extra ((&&&))
+import GHC.Generics (Generic)
 import System.Environment
 import System.Exit
 
 main :: IO ()
 main =
     getArgs >>= \case
-        [p] -> main' p
-        _ -> T.putStrLn "Expected one argument, the input file path"
+        [p, p'] -> main' p p'
+        _ -> T.putStrLn "Expected two argument, the input and output file paths"
 
-main' :: FilePath -> IO ()
-main' p = do
-    fileContents <- BL.readFile p
+main' :: FilePath -> FilePath -> IO ()
+main' inPath outPath = do
+    fileContents <- BL.readFile inPath
     (_header, matches) <-
         either (const $ T.putStrLn "Error parsing CSV" >> exitFailure) (pure . second toList)
-            . decodeByName @Match
+            . decodeByName
             $ stripUtf8Bom fileContents
-    for_ matches \match ->
-        T.putStrLn $
-            match.homeTeam.unwrap
-                <> " vs "
-                <> match.awayTeam.unwrap
-                <> ": "
-                <> showT match.homeGoals
-                <> "-"
-                <> showT match.awayGoals
+    BL.writeFile outPath $ encode $ processMatches matches
+
+processMatches :: [Match] -> [TableRow]
+processMatches =
+    sortOn (Down . ((.points) &&& (.goalDifference)))
+        . map
+            ( \(team, TableRowRaw{..}) ->
+                TableRow
+                    { team
+                    , played = wins + draws + losses
+                    , points = fromIntegral wins * 3 + fromIntegral draws
+                    , goalDifference = fromIntegral for - fromIntegral against
+                    , ..
+                    }
+            )
+        . Map.toList
+        . flip
+            foldl'
+            mempty
+            ( flip \m ->
+                id
+                    . ( let
+                            f r =
+                                let r' = r{for = r.for + m.homeGoals, against = r.against + m.awayGoals} :: TableRowRaw
+                                 in case m.result of
+                                        HomeWin -> r'{wins = r.wins + 1} :: TableRowRaw
+                                        Draw -> r'{draws = r.draws + 1} :: TableRowRaw
+                                        AwayWin -> r'{losses = r.losses + 1} :: TableRowRaw
+                         in
+                            flip Map.alter m.homeTeam \case
+                                Nothing -> Just $ f $ TableRowRaw 0 0 0 0 0
+                                Just r -> Just $ f r
+                      )
+                    . ( let
+                            f r =
+                                let r' = r{for = r.for + m.awayGoals, against = r.against + m.homeGoals} :: TableRowRaw
+                                 in case m.result of
+                                        AwayWin -> r'{wins = r.wins + 1} :: TableRowRaw
+                                        Draw -> r'{draws = r.draws + 1} :: TableRowRaw
+                                        HomeWin -> r'{losses = r.losses + 1} :: TableRowRaw
+                         in
+                            flip Map.alter m.awayTeam \case
+                                Nothing -> Just $ f $ TableRowRaw 0 0 0 0 0
+                                Just r -> Just $ f r
+                      )
+            )
+data TableRow = TableRow
+    { team :: Team
+    , played :: Word
+    , points :: Int
+    , goalDifference :: Int
+    , wins :: Word
+    , draws :: Word
+    , losses :: Word
+    , for :: Word
+    , against :: Word
+    }
+    deriving (Show, Generic, ToRecord)
+data TableRowRaw = TableRowRaw
+    { wins :: Word
+    , draws :: Word
+    , losses :: Word
+    , for :: Word
+    , against :: Word
+    }
+    deriving (Show)
 
 data Match = Match
     { league :: Text
@@ -79,11 +149,8 @@ data Result
     | Draw
     | AwayWin
     deriving (Show)
-newtype Team = Team {unwrap :: Text}
-    deriving newtype (Eq, Ord, Show)
-
-showT :: (Show a) => a -> Text
-showT = T.pack . show
+newtype Team = Team Text
+    deriving newtype (Eq, Ord, Show, ToField)
 
 -- https://github.com/haskell-hvr/cassava/issues/106
 stripUtf8Bom :: BL.ByteString -> BL.ByteString
