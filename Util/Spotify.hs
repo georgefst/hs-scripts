3b7ab7c209c8c6b76332bb937126e9e148dcf1b1
George Thomas <georgefsthomas@gmail.com>
2025-05-20 00:27:51 +0100

    Use Servant API type from Spotify library
    For now, we don't define wrapped versions for the endpoints we don't use.
    
    We'd prefer to use records rather than positional matching, but that doesn't yet work with `servant-flatten`.

--- Util/Spotify.hs
+++ Util/Spotify.hs
@@ -9,19 +9,12 @@ module Util.Spotify (
 ) where
 
 import Data.Proxy (Proxy (Proxy))
-import Servant.API ((:>), type (:<|>) ((:<|>)))
+import Servant.API (type (:<|>) ((:<|>)))
 import Servant.API.Flatten (flatten)
 import Servant.Client.JS (BaseUrl (BaseUrl), ClientEnv (ClientEnv), Scheme (Https), client, runClientM)
 import Spotify (marketFromToken)
-import Spotify.Servant.Albums (GetAlbum)
-import Spotify.Servant.Core (AuthHeader, handleAllJSONOrNoContent)
-import Spotify.Servant.Player (GetPlaybackState)
-
-type API =
-    AuthHeader
-        :> ( GetPlaybackState
-                :<|> GetAlbum
-           )
+import Spotify.Servant (API)
+import Spotify.Servant.Core (handleAllJSONOrNoContent)
 
 -- TODO leading slash can be dropped with https://github.com/morganthomas/servant-client-js/pull/7
 run = flip runClientM $ ClientEnv $ BaseUrl Https "api.spotify.com" 443 "/v1"
@@ -29,6 +22,33 @@ run = flip runClientM $ ClientEnv $ BaseUrl Https "api.spotify.com" 443 "/v1"
 getPlaybackState t = run $ handleAllJSONOrNoContent <$> getPlaybackState' t marketFromToken (Just "episode")
 getAlbum a t = run $ getAlbum' t a marketFromToken
 
-getPlaybackState'
-    :<|> getAlbum' =
+getAlbum'
+    :<|> _getAlbumTracks'
+    :<|> _removeAlbums'
+    :<|> _getArtist'
+    :<|> _getCategories'
+    :<|> _getEpisode'
+    :<|> _getSavedEpisodes'
+    :<|> _saveEpisodes'
+    :<|> _removeEpisodes'
+    :<|> getPlaybackState'
+    :<|> _transferPlayback'
+    :<|> _getAvailableDevices'
+    :<|> _getCurrentlyPlayingTrack'
+    :<|> _startPlayback'
+    :<|> _pausePlayback'
+    :<|> _skipToNext'
+    :<|> _skipToPrevious'
+    :<|> _seekToPosition'
+    :<|> _getPlaylist'
+    :<|> _addToPlaylist'
+    :<|> _getMyPlaylists'
+    :<|> _createPlaylist'
+    :<|> _getSearch'
+    :<|> _getTrack'
+    :<|> _getSavedTracks'
+    :<|> _saveTracks'
+    :<|> _removeTracks'
+    :<|> _getMe'
+    :<|> _getUser' =
         client $ flatten $ Proxy @API
