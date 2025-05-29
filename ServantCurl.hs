c0cce5aa0d5429662773173b7d659a9b5cc678bd
George Thomas <georgefsthomas@gmail.com>
2025-05-29 20:50:04 +0100

    copied

---
+++ ServantCurl.hs
@@ -0,0 +1,144 @@
+-- https://docs.servant.dev/en/stable/cookbook/curl-mock/CurlMock.html
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeOperators #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
+{-# HLINT ignore "Use newtype instead of data" #-}
+
+import Control.Lens ((^.))
+import Data.Aeson
+import Data.Aeson.Text
+import Data.Proxy (Proxy (Proxy))
+import Data.Text (Text)
+import Data.Text qualified as T
+import Data.Text.Encoding (decodeUtf8)
+import Data.Text.IO qualified as T.IO
+import Data.Text.Lazy qualified as LazyT
+import GHC.Generics
+import Servant (
+    Get,
+    JSON,
+    Post,
+    ReqBody,
+    (:<|>),
+    (:>),
+ )
+import Servant.Foreign (
+    Foreign,
+    GenerateList,
+    HasForeign,
+    HasForeignType,
+    Req,
+    Segment,
+    SegmentType (Cap, Static),
+    argName,
+    listFromAPI,
+    path,
+    reqBody,
+    reqMethod,
+    reqUrl,
+    typeFor,
+    unPathSegment,
+    unSegment,
+ )
+import Test.QuickCheck
+import Test.QuickCheck.Gen (generate)
+
+type UserAPI =
+    "users" :> Get '[JSON] [User]
+        :<|> "new" :> "user" :> ReqBody '[JSON] User :> Post '[JSON] ()
+
+data User = User
+    { name :: String
+    , age :: Int
+    , email :: String
+    }
+    deriving (Eq, Show, Generic)
+
+instance Arbitrary User where
+    arbitrary = do
+        name <- arbitrary
+        age <- arbitrary
+        email <- arbitrary
+        pure User{..}
+    shrink = genericShrink
+instance ToJSON User
+instance FromJSON User
+
+api :: Proxy UserAPI
+api = Proxy
+
+data NoLang
+
+data Mocked = Mocked (IO Text)
+
+instance (ToJSON a, Arbitrary a) => HasForeignType NoLang Mocked a where
+    typeFor _ _ _ =
+        Mocked (genText (Proxy :: Proxy a))
+
+genText :: (ToJSON a, Arbitrary a) => Proxy a -> IO Text
+genText p =
+    fmap (\v -> LazyT.toStrict $ encodeToLazyText v) (genArb p)
+
+genArb :: (Arbitrary a) => Proxy a -> IO a
+genArb _ =
+    generate arbitrary
+
+generateCurl ::
+    (GenerateList Mocked (Foreign Mocked api), HasForeign NoLang Mocked api) =>
+    Proxy api ->
+    Text ->
+    IO Text
+generateCurl p host =
+    fmap T.unlines body
+  where
+    body =
+        mapM (generateEndpoint host) $
+            listFromAPI (Proxy :: Proxy NoLang) (Proxy :: Proxy Mocked) p
+
+generateEndpoint :: Text -> Req Mocked -> IO Text
+generateEndpoint host req =
+    case maybeBody of
+        Just body ->
+            body >>= \b ->
+                return $
+                    T.intercalate
+                        " "
+                        [ "curl"
+                        , "-X"
+                        , method
+                        , "-d"
+                        , "'" <> b <> "'"
+                        , "-H 'Content-Type: application/json'"
+                        , host <> "/" <> url
+                        ]
+        Nothing ->
+            return $ T.intercalate " " ["curl", "-X", method, host <> "/" <> url]
+  where
+    method = decodeUtf8 $ req ^. reqMethod
+
+    url = T.intercalate "/" $ map segment (req ^. reqUrl . path)
+
+    maybeBody = fmap (\(Mocked io) -> io) (req ^. reqBody)
+
+segment :: Segment Mocked -> Text
+segment seg =
+    case unSegment seg of
+        Static p ->
+            unPathSegment p
+        Cap arg ->
+            -- Left as exercise for the reader: Mock args in the url
+            unPathSegment $ arg ^. argName
+
+main :: IO ()
+main =
+    generateCurl api "localhost:8081" >>= T.IO.putStrLn
