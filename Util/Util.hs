77946e3e6fe2e56a50f381afc49d4ddb47f69c84
George Thomas <georgefsthomas@gmail.com>
2025-07-02 21:51:32 +0100

    Add `outerProduct` to utils

--- Util/Util.hs
+++ Util/Util.hs
@@ -5,6 +5,7 @@
 module Util.Util where
 
 import Data.Bool (bool)
+import Data.Functor
 import Data.Text (Text)
 import Data.Text.IO qualified as T
 
@@ -13,3 +14,6 @@ modifyFile f file = T.writeFile file . f =<< T.readFile file
 
 ($?) :: Bool -> (a -> a) -> a -> a
 ($?) = flip $ bool id
+
+outerProduct :: (a -> b -> c) -> [a] -> [b] -> [[c]]
+outerProduct f xs ys = xs <&> \x -> ys <&> \y -> f x y
