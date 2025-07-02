87d992310cd7a58a8516bd2597124aeed8528fa3
George Thomas <georgefsthomas@gmail.com>
2025-07-02 21:56:55 +0100

    Add `SpotNumber` script
    https://news.ycombinator.com/item?id=36214911

---
+++ SpotNumber.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE BlockArguments #-}
+{-# OPTIONS_GHC -Wall #-}
+
+module SpotNumber (main) where
+
+import Data.Foldable
+import Data.Traversable
+import System.Console.ANSI
+import System.Random
+import Util.Util
+
+main :: IO ()
+main = do
+    Just d@(h, w) <- getTerminalSize
+    pDigit <- randomPos d
+    let ps = outerProduct (flip (,)) [0 .. h - 1] [0 .. w - 1]
+    grid <- for ps $ traverse \p -> if p == pDigit then randomDigit else randomAlpha
+    for_ grid \cs -> for_ cs putChar >> putChar '\n'
+
+randomDigit :: IO Char
+randomDigit = randomRIO ('0', '9')
+randomAlpha :: IO Char
+randomAlpha = randomRIO ('A', 'Z')
+randomPos :: (Int, Int) -> IO (Int, Int)
+randomPos (w, h) = (,) <$> randomRIO (0, w - 1) <*> randomRIO (0, h - 1)
