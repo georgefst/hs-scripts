1c08951a7c146ad74c3ee04b794e56e6a9965c60
George Thomas <georgefsthomas@gmail.com>
2025-07-03 01:22:04 +0100

    Final cleanup

--- SpotNumber.hs
+++ SpotNumber.hs
@@ -16,23 +16,23 @@ import Util.Util
 
 main :: IO ()
 main = do
+    dims@(_, h) <- maybe (error "couldn't get terminal size") swap <$> getTerminalSize
+    pDigit@(x, y) <- randomPos dims
+    chars <- for (mkGrid dims) $ traverse $ bool randomAlpha randomDigit . (== pDigit)
+    sequence_ $ intersperse (putChar '\n') $ map (traverse_ putChar) chars
     hideCursor
-    dims <- maybe (5, 3) swap <$> getTerminalSize
-    pDigit <- randomPos dims
-    chars <- for (mkGrid dims) $ traverse $ (flip fmap . bool randomAlpha randomDigit <*> (,)) . (== pDigit)
-    let printRow f = traverse_ $ f $ putChar . snd
-    sequence_ $ intersperse (putChar '\n') $ map (printRow id) chars
-    cursorUp $ snd dims - 1
+    cursorUp $ h - 1
     void getLine
     cursorUp 1
-    cursorDown $ snd pDigit
-    cursorForward $ fst pDigit
+    cursorDown y
+    cursorForward x
     clearFromCursorToLineEnd
-    drop (fst pDigit) (chars !! snd pDigit) & printRow \f x@(b, _) -> do
-        when b $ setSGR [SetColor Foreground Vivid Red]
-        f x
-        when b $ setSGR []
-    cursorDown $ snd dims
+    (uncons . drop x =<< chars !? y) & traverse_ \(c, cs) -> do
+        setSGR [SetColor Foreground Vivid Red]
+        putChar c
+        setSGR []
+        for_ cs putChar
+    cursorDown h
     void getLine
     showCursor
 
