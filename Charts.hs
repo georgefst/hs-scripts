bddfa28d4413703f0b66cdb109293f36eeaf96ce
George Thomas <georgefsthomas@gmail.com>
2025-07-21 16:54:38 +0100

    graph examples with explicit coords

--- Charts.hs
+++ Charts.hs
@@ -1,13 +1,15 @@
 {-# LANGUAGE GHC2024 #-}
 {-# LANGUAGE BlockArguments #-}
-{-# OPTIONS_GHC -Wall #-}
+-- {-# OPTIONS_GHC -Wall #-}
 {-# OPTIONS_GHC -Wno-missing-signatures #-}
 {-# OPTIONS_GHC -Wno-type-defaults #-}
 
 module Charts (main) where
 
 import Control.Monad
+import Data.Bifunctor
 import Data.Foldable
+import Data.List
 import Graphics.Rendering.Chart
 import Graphics.Rendering.Chart.Backend.Cairo
 import Graphics.Rendering.Chart.Easy hiding (tan)
@@ -21,11 +23,45 @@ main = do
         . fillBackground def
         $ toRenderable chart
 
+-- it looks linear
+-- tree has about 100 nodes, each with this many classes
+-- so around 1ms per class/attr in Wasm on a very beefy machine
 chart :: Layout Double Double
 chart = execEC $ do
-    layout_x_axis . laxis_generate .= scaledAxis def (lower, upper)
-    for_ functions \(l, f) ->
-        plot $ line l [[(x, f x) | x <- [lower + 0.001, lower + ((upper - lower) / increments) .. upper]]]
+    -- layout_x_axis . laxis_generate .= scaledAxis def (lower, upper)
+    plot $
+        line
+            "f"
+            [ -- [ (0, 0.25)
+              -- , (5, 0.55)
+              -- , (10, 0.8)
+              -- , (20, 1.3)
+              -- , (50, 5.3)
+              -- , (100, 10.1)
+              -- ]
+
+              -- 2-5 pretty much interchangeable, big drop after top 7
+              map (second (15 -))
+                . sortOn fst
+                $ zip
+                    [ 4
+                    , 3
+                    , 5
+                    , 1
+                    , 2
+                    , 10
+                    , 8
+                    , 7
+                    , 11
+                    , 6
+                    , 9
+                    , 12
+                    , 15
+                    , 14
+                    , 13
+                    ]
+                    [1 ..]
+            ]
 
 increments = 500
 (lower, upper) = (-10, 10)
