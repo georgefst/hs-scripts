e7d809eda198ff72b4b9e6deaecd8be784738372
George Thomas <georgefsthomas@gmail.com>
2025-09-13 13:09:26 +0100

    Add basic Nix flake

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/25.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            ((pkgs.haskellPackages.override {
+              overrides = self: super: { };
+            }).ghcWithPackages (hpkgs: with hpkgs; [
+              colour
+              directory
+              extra
+              JuicyPixels
+              lucid2
+              pretty-simple
+              shake
+              text
+              unix
+            ]))
+            cabal-install
+            fourmolu
+            haskell-language-server
+          ];
+        };
+      });
+}
