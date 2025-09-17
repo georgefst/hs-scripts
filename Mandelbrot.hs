8f2b5d3ea212c2ae30306c7c58715267a4be5446
George Thomas <georgefsthomas@gmail.com>
2025-09-17 19:44:56 +0100

    Add CLI args for Mandelbrot window and resolution

--- Mandelbrot.hs
+++ Mandelbrot.hs
@@ -1,4 +1,7 @@
+{-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE LexicalNegation #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
 {-# OPTIONS_GHC -Wall #-}
 {-# OPTIONS_GHC -Wno-missing-signatures #-}
 {-# OPTIONS_GHC -Wno-type-defaults #-}
@@ -11,11 +14,17 @@ import Data.Colour.SRGB
 import Data.Complex
 import Data.List
 import Data.Tuple.Extra
+import Options.Generic
 
-width = 500
-height = 500
-centre = (-0.5, 0)
-size = 3
+data Opts = Opts
+    { width :: Int
+    , height :: Int
+    , centreX :: Double
+    , centreY :: Double
+    , size :: Double
+    }
+    deriving (Eq, Ord, Show, Generic, ParseRecord)
+
 bound = 2
 maxIterations = 50
 power = 2
@@ -34,16 +43,18 @@ divergenceIterations c = findIndex ((>= (bound ^ 2)) . magnitudeSquared) . take 
   where
     magnitudeSquared (x :+ y) = x * x + y * y
 
-main =
+main = do
+    Opts{..} <- getRecord ""
+    let
+        (xMin, xMax) = ((- size / 2) &&& (+ size / 2)) centreX
+        (yMin, yMax) = ((- size / 2) &&& (+ size / 2)) centreY
+        pixelToComplex (x, y) =
+            (fromIntegral x / fromIntegral width * (xMax - xMin) + xMin)
+                :+ (fromIntegral y / fromIntegral height * (yMin - yMax) + yMax)
     writePng "mandelbrot.png" $
         generateImage
             (curry $ convertColour . maybe setColour iterationsToColour . divergenceIterations . pixelToComplex)
             width
             height
   where
-    (xMin, xMax) = ((- size / 2) &&& (+ size / 2)) $ fst centre
-    (yMin, yMax) = ((- size / 2) &&& (+ size / 2)) $ snd centre
-    pixelToComplex (x, y) =
-        (fromIntegral x / fromIntegral width * (xMax - xMin) + xMin)
-            :+ (fromIntegral y / fromIntegral height * (yMin - yMax) + yMax)
     convertColour (RGB r g b) = PixelRGB8 (floor $ 255 * r) (floor $ 255 * g) (floor $ 255 * b)
